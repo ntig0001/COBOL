6ac30a38b5d908761887aadefdfac101ac7688fe
ntigi <ntig0001@algonquinlive.com>
2020-08-26 16:46:09 -0400

    initial commit

---
+++ Dummy_Project/dummy.cbl
@@ -0,0 +1,14 @@
+       program-id. dummy as "dummy".
+
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+
+       procedure division.
+           display "Hello There! This is a dummy project".
+       
+           goback.
+
+       end program dummy.
