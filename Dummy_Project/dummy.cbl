e747436bb69171912c0ba57852fde814f73d5ed3
ntigi <ntig0001@algonquinlive.com>
2020-08-26 17:05:49 -0400

    initial commit

--- Dummy_Project/dummy.cbl
+++ Dummy_Project/dummy.cbl
@@ -7,8 +7,8 @@
        working-storage section.
 
        procedure division.
-           display "Hello There! This is a dummy project".
-       
+           display "Hello World. I am a dummy project!".
+
            goback.
 
        end program dummy.
